ad18db8860320e66326355eda4314727f18b1859
Seth <Chosenblades@gmail.com>
2023-04-28 20:38:29 -0600

    remove crypto package, change host to '::'

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.addHook('onClose', (instance, done) => {
     done();
 });
 
-app.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' }, (err) => {
+app.listen({ port: process.env.PORT || 3000, host: '::' }, (err) => {
     if(err) {
         app.log.error(err);
         process.exit(1);
